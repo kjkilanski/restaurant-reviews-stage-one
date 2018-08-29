7283d7057fbfd5aaa07398dfeccda8d3c3fb4f5e
Kelley J Kilanski <17366192+kjkilanski@users.noreply.github.com>
2018-08-29 10:21:21 -0700

    Chore: update service worker registration

--- js/main.js
+++ js/main.js
@@ -199,18 +199,12 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 
 }
 
-// https://developers.google.com/web/fundamentals/primers/service-workers/
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+    navigator.serviceWorker.register('/sw.js');
   });
 }
+
 
 
 /* addMarkersToMap = (restaurants = self.restaurants) => {
